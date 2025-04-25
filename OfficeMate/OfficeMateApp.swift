2ca69cb1cd321cf1c5a2f16b8b783da75eb84f6a
王增凤 <wangzengfeng@iMac.hub>
2025-04-25 12:48:08 +1000

    Initial commit

---
+++ OfficeMate/OfficeMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  OfficeMateApp.swift
+//  OfficeMate
+//
+//  Created by 王增凤 on 25/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct OfficeMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OfficeMateTests/OfficeMateTests.swift
@@ -0,0 +1,17 @@
+//
+//  OfficeMateTests.swift
+//  OfficeMateTests
+//
+//  Created by 王增凤 on 25/4/2025.
+//
+
+import Testing
+@testable import OfficeMate
+
+struct OfficeMateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
